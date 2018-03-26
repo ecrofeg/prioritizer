95f36b97dd2b8a32f6e9297d6a0e8348923d6102
pnaumov <ecrofeg1@gmail.com>
2018-03-26 14:20:44 +0300

    Get issues list

--- app/Http/Controllers/IssuesController.php
+++ app/Http/Controllers/IssuesController.php
@@ -7,6 +7,14 @@ use GuzzleHttp\Client;
 
 class IssuesController extends Controller
 {
+	protected $prioritiesWeight = [
+		'3' => 0,
+		'4' => 1,
+		'5' => 2,
+		'6' => 3,
+		'7' => 4
+	];
+	
 	protected $client = null;
 	
 	public function __construct() {
@@ -19,16 +27,69 @@ class IssuesController extends Controller
 	}
 	
 	public function index() {
-		return $this->makeRedmineRequest('issues');
+		try {
+			$user = $this->getCurrentUser();
+			$response = $this->makeRedmineRequest('issues');
+			
+			if (isset($response->issues)) {
+				$issues = $response->issues;
+				
+				usort($issues, function ($a, $b) {
+					return $this->getPriorityForIssue($b) <=> $this->getPriorityForIssue($a);
+				});
+				
+				return $issues;
+			}
+			else {
+				throw new \Exception('No issues found');
+			}
+		}
+		catch (\Exception $exception) {
+			return $this->error($exception->getMessage());
+		}
+	}
+	
+	/**
+	 * @param \stdClass $issue
+	 *
+	 * @return int
+	 */
+	protected function getPriorityForIssue(\stdClass $issue): int {
+		$result = $this->prioritiesWeight[$issue->priority->id];
+		
+		return $result;
+	}
+	
+	/**
+	 * @param string $text
+	 *
+	 * @return array
+	 */
+	protected function error(string $text) {
+		return abort(500, $text);
+	}
+	
+	/**
+	 * @return object
+	 * @throws \Exception
+	 */
+	protected function getCurrentUser(): object {
+		$response = $this->makeRedmineRequest('users/current');
+		
+		if (!isset($response->user)) {
+			throw new \Exception('Unauthorized');
+		}
+		
+		return $response->user;
 	}
 	
 	/**
 	 * @param string $uri
 	 * @param array $params
 	 *
-	 * @return \Psr\Http\Message\StreamInterface
+	 * @return object
 	 */
-	protected function makeRedmineRequest(string $uri, array $params = []) {
+	protected function makeRedmineRequest(string $uri, array $params = []): object {
 		$defaultParams = [
 			'assigned_to_id' => 'me'
 		];
@@ -39,6 +100,6 @@ class IssuesController extends Controller
 			'query' => $requestParams,
 		]);
 		
-		return $response->getBody();
+		return json_decode($response->getBody()->getContents());
 	}
 }
