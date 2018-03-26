276801205c5f3992af568c7e1f3b6cdc9a51f1e6
pnaumov <ecrofeg1@gmail.com>
2018-03-26 13:43:33 +0300

    Curl client initialized

--- app/Http/Controllers/IssuesController.php
+++ app/Http/Controllers/IssuesController.php
@@ -2,15 +2,43 @@
 
 namespace App\Http\Controllers;
 
+// use Illuminate\Support\Facades\Redis;
+use GuzzleHttp\Client;
+
 class IssuesController extends Controller
 {
-    public function index() {
-    	return [
-    		'123' => '123'
-		];
+	protected $client = null;
+	
+	public function __construct() {
+		$this->client = new Client([
+			'base_uri' => env('REDMINE_API_URL'),
+			'headers' => [
+				'X-Redmine-API-Key' => env('REDMINE_API_KEY')
+			]
+		]);
 	}
 	
-	protected function makeRedmineRequest($params) {
- 
+	public function index() {
+		return $this->makeRedmineRequest('issues');
+	}
+	
+	/**
+	 * @param string $uri
+	 * @param array $params
+	 *
+	 * @return \Psr\Http\Message\StreamInterface
+	 */
+	protected function makeRedmineRequest(string $uri, array $params = []) {
+		$defaultParams = [
+			'assigned_to_id' => 'me'
+		];
+		
+		$requestParams = array_merge($defaultParams, $params);
+		
+		$response = $this->client->get($uri . '.json', [
+			'query' => $requestParams,
+		]);
+		
+		return $response->getBody();
 	}
 }
