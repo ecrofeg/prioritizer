ad88e8936492c3b495aaf75f37b3faf72168d8c5
ecrofeg <ecrofeg1@gmail.com>
2018-06-06 13:28:16 +0400

    Sexy styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Test</title>
+	<title>prioritizer</title>
 	<link rel="stylesheet" href="./prioritizer.min.css">
 </head>
 <body>
